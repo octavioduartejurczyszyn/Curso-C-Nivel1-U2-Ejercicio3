6982e95ed171e5d7ca0d483f7e3810935ecb2280
octavioduartejurczyszyn <tatoduarte9@gmail.com>
2024-02-20 12:57:19 +0100

    Primera subida

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float kilometros, velocidad, tiempo;
+            Console.WriteLine("Escriba los kilómetros existentes entre dos ciudades:");
+            kilometros = float.Parse(Console.ReadLine());
+            Console.WriteLine("Escriba la velocidad promedio de un vehículo");
+            velocidad = float.Parse(Console.ReadLine());
+            tiempo = kilometros / velocidad;
+            Console.WriteLine("El tiempo aproximado de llegar de una ciudad a otra es: " + tiempo.ToString("0.00") + " horas");
+        }
+    }
+}
